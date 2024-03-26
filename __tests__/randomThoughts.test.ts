772434ba295916dff7251a5497ee84301984e9bf
anaaragon88 <ana.aragon88@gmail.com>
2024-03-26 12:35:49 +0100

    correct test

--- __tests__/randomThoughts.test.ts
+++ __tests__/randomThoughts.test.ts
@@ -1,14 +1,19 @@
 import request from "supertest";
+import ThoughtModel from "../models/ThoughtModel";
 
 import { app } from "../app";
 
 test("should return 201 status code when a new thought has been created", async () => {
 
     const newThought = {
-        title: "Test",
+        name: "Test",
         description: "test"
     }
 
     const response = await request(app).post("/thoughts").send(newThought);
     expect(response.status).toBe(201);
 })
+
+// afterAll(async() =>{
+//     await ThoughtModel.destroy({where:{ name: 'test'}})
+// })
